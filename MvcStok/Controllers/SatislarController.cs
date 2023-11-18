418bc5462d4ca1eb940b27efa0e1f169becad765
alpykb <alpyakaba48@gmail.com>
2023-11-19 00:44:33 +0300

    sales transactions

--- MvcStok/Controllers/SatislarController.cs
+++ MvcStok/Controllers/SatislarController.cs
@@ -19,13 +19,51 @@ namespace MvcStok.Controllers
         [HttpGet]
         public ActionResult YeniSatis()
         {
+            //Ürünler
+            List<SelectListItem> urun = (from x in db.tblurunler.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.ad,
+                                             Value = x.id.ToString()
+                                         }).ToList();
+            ViewBag.drop1 = urun;
+
+
+            //Personel
+            List<SelectListItem> per = (from x in db.tblpersonel.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad + " " + x.soyad,
+                                            Value = x.id.ToString()
+                                        }).ToList();
+            ViewBag.drop2 = per;
+
+            //Müşteriler
+            List<SelectListItem> must = (from x in db.tblmusteri.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.ad + " " + x.soyad,
+                                             Value = x.id.ToString()
+                                         }).ToList();
+            ViewBag.drop3 = must;
+
             return View();
         }
 
         [HttpPost]
         public ActionResult YeniSatis(tblsatislar p)
         {
-            return View();
+            var urun = db.tblurunler.Where(x => x.id == p.tblurunler.id).FirstOrDefault();
+            var musteri = db.tblmusteri.Where(x => x.id == p.tblmusteri.id).FirstOrDefault();
+            var personel = db.tblpersonel.Where(x => x.id == p.tblpersonel.id).FirstOrDefault();
+            p.tblurunler = urun;
+            p.tblmusteri = musteri;
+            p.tblpersonel = personel; 
+            p.tarih= DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.tblsatislar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
         }
     }
 }
