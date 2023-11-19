f6ad452428716b1100c0b879c7089fcaa00b5153
alpykb <alpyakaba48@gmail.com>
2023-11-19 23:42:06 +0300

    login screen edit.

--- MvcStok/Controllers/GirisYapController.cs
+++ MvcStok/Controllers/GirisYapController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcStok.Models.Entity;
+using System.Web.Security;
 namespace MvcStok.Controllers
 {
     public class GirisYapController : Controller
     {
         // GET: GirisYap
+        DbMvcStokEntities1 db = new DbMvcStokEntities1();
         public ActionResult Giris()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Giris(tbladmin t)
+        {
+            var bilgiler = db.tbladmin.FirstOrDefault(x => x.kullanici == t.kullanici && x.sifre == t.sifre);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.kullanici,false);
+                return RedirectToAction("Index", "Musteri");
+            }
+            else
+            {
+                return View();
+            }
+           
         }
     }
 }
